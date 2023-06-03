c6f6cde6524de3a80cada8e6590a4887c6e052c1
Sonick Mumba <kingellie.mumba@gmail.com>
2023-06-03 18:28:03 +0200

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './navbar.css';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 
 const Navbar = () => {
   return (
-    <div>Nav</div>
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+
+      </div>
+    </div>
   )
 }
 
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './header.css';
 
+
+
+
+// BEM -> Block Element Modifier type of naming css
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+
+      </div>
+    </div>
   )
 }
 
