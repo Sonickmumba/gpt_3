0bcb8c9843b8a2c869b554fda7a62cf5e1438edb
Sonick Mumba <kingellie.mumba@gmail.com>
2023-06-06 23:59:31 +0200

    Remove comments

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './navbar.css';
 import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
 
-// BEM -> bLOCK ELEM
+// BEM -> bLOCK 
 const Navbar = () => {
   return (
     <div className='gpt3__navbar'>
